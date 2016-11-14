373a79c1af106a6b2117e20f317237016eaebd90
Aaron W. Steele <aaronwsteele@Aarons-MacBook-Air.local>
2016-11-14 13:41:56 -0800

    add cards via JS and commented out HTML .card divs

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,12 @@
 var cardOne = "queen", cardTwo = "king", cardThree = "queen", cardFour = "king";
+
+var createCard = function(num){
+	var board = document.getElementById('game-board');
+	for(var i = 0; i < num; i++){
+		var card = document.createElement('div');
+		card.className = 'card';
+		board.appendChild(card);
+	}
+}
+
+createCard(4);
